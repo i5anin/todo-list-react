fef9ce4b2da4160a1e6ee17348cf2a0f33996e1a
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2025-08-17 20:50:28 +0300

    chore!(repo): заменить исходный проект
    - замена структуры проекта на актуальную заготовку
    - добавлен локальный json-server (скрипт "server", источник src/db.json, порт 3003)
    - исправлен POST /todos и обработка статусов ответа (res.ok + корректный парсинг)
    - подключено создание todo из UI к createTodosAsync с последующим обновлением списка
    - инструкции по запуску для Windows (npm run server, проверка через Invoke-RestMethod)
    
    BREAKING CHANGE: прежние скрипты и структура несовместимы; требуется запуск json-server локально

--- src/features/todo/delete/useDeleteTodo.js
+++
@@ -1,7 +0,0 @@
-import { deleteTodo } from '@/shared/api/todos.js'
-
-export function useDeleteTodo(fetchTodos) {
-    return function handleDelete(id) {
-        deleteTodo(id).then(fetchTodos)
-    }
-}
--- src/features/todo/toggle/useToggleTodo.js
+++
@@ -1,9 +0,0 @@
-
-
-import { updateTodo } from '@/shared/api/todos.js'
-
-export function useToggleTodo(fetchTodos) {
-    return function toggle(todo) {
-        updateTodo(todo.id, { completed: !todo.completed }).then(fetchTodos)
-    }
-}
